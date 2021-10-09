19feb03aa3b02d9bac576efeece19771d107eff0
JasonWozniak <49988328+JasonWozniak@users.noreply.github.com>
2021-10-09 15:09:55 -0700

    $1

---
+++ Project1-Backend/src/main/java/servlets/ServletManager.java
@@ -0,0 +1,18 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletManager extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+
+}
