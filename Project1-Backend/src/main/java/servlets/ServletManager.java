dc956a09eff989d6ba8fbd8127bb03e7ebb65e95
andrewdpetersen <andrew.petersen@revature.net>
2021-10-09 22:22:09 -0500

    Updated and tested Cancel ticket HTML form: currently working and hitting the server

--- Project1-Backend/src/main/java/servlets/ServletManager.java
+++ Project1-Backend/src/main/java/servlets/ServletManager.java
@@ -3,11 +3,14 @@ package servlets;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class ServletManager extends HttpServlet {
     @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp){
-        req.getParameter("ticket_id");
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String string_ticket_id = req.getParameter("ticket_id");
+        int ticket_id = Integer.parseInt(string_ticket_id);
+        resp.getWriter().print("Ticket #: "+ticket_id + " has been cancelled");
         resp.setStatus(200);
     }
 
