b63037708d1f694c767f499efc5ca84adb714a55
andrewdpetersen <90648378+andrewdpetersen@users.noreply.github.com>
2021-10-10 19:51:27 -0500

    Revert "Da ofor testing"

--- Project1-Backend/src/main/java/servlets/ServletManager.java
+++ Project1-Backend/src/main/java/servlets/ServletManager.java
@@ -1,7 +1,5 @@
 package servlets;
 
-import daos.TicketDAO;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,16 +10,8 @@ public class ServletManager extends HttpServlet {
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String string_ticket_id = req.getParameter("ticket_id");
         int ticket_id = Integer.parseInt(string_ticket_id);
-        try{
-            TicketDAO dao = new TicketDAO();
-            int flight_id = dao.delete(ticket_id);
-            resp.getWriter().println("Ticket #: "+ticket_id + " has almost been cancelled");
-            resp.getWriter().print("You no longer have a ticket for flight number: "+flight_id);
-            resp.setStatus(200);
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+        resp.getWriter().print("Ticket #: "+ticket_id + " has been cancelled");
+        resp.setStatus(200);
     }
 
     @Override
