edf1b106172c3fe617f18eee8a0cddab44e883cd
JasonWozniak <49988328+JasonWozniak@users.noreply.github.com>
2021-10-18 13:40:20 -0700

    Added FileLogger area to put approved code into

---
+++ Project1-Backend/src/main/java/utils/FileLogger.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class FileLogger {
+
+    //write to a file FileWriter... create a filename dynamically w/ datestamp
+    //find where the error Exception occurs
+    //timestamp / datestamp
+    //format how we write it.
+    
+
+
+
+
+}
