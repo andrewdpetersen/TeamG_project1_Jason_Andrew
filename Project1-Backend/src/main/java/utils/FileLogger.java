8da6c60cf2a934d39b8c471b74df63bb11243a0a
JasonWozniak <49988328+JasonWozniak@users.noreply.github.com>
2021-10-19 14:38:23 -0700

    added Javascript userTicket

--- Project1-Backend/src/main/java/utils/FileLogger.java
+++ Project1-Backend/src/main/java/utils/FileLogger.java
@@ -61,7 +61,7 @@ public class FileLogger {
         return fileLogger;
     }
 
-    // 
+    //
     //write to a file FileWriter... create a filename dynamically w/ datestamp
     //find where the error Exception occurs
     //timestamp / datestamp
