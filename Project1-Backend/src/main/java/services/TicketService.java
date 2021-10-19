558cb73e841becca315b3b04b76bfa3d3dbdbb79
andrewdpetersen <andrew.petersen@revature.net>
2021-10-19 14:16:18 -0500

    cleaned up ticket service, added dependency to pom.xml, added logic to test JS in ticketServlet

--- Project1-Backend/src/main/java/services/TicketService.java
+++ Project1-Backend/src/main/java/services/TicketService.java
@@ -1,5 +1,11 @@
 package services;
 
+import models.Tickets_People_Flights;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
 public class TicketService {
     // we want 2 private fields Session factory, session
     private static SessionFactory sfact;  // so we can get the session
@@ -7,30 +13,30 @@ public class TicketService {
 
 
     //User - buy a TICKET or multiple tickets
-    public static void buyNewTicket (tickets_people_flights tpf){
+    public static void buyNewTicket (Tickets_People_Flights tpf){
         // if(locked=false) then proceed... going to implement before hibernate... a validator somewhere
         sess.save(tpf);}  // Use the session to save the Object tpf into database
 
     //User - buy MULTIPLE tickets
-    public static void buyMultiTicket(List<tickets_people_flights> tpf) {
+    public static void buyMultiTicket(List<Tickets_People_Flights> tpf) {
         //hardest due to calling multiple hibernate methods??
         //iterate over the List of tickets and save each one to the database
     }
 
     //User - CANCEL a ticket // Admin will also use
-    public static void cancelTicket (ticket_people_flights tpf){sess.delete(tpf)} // Use the session to delete the Object tpf from database
+    public static void cancelTicket (Tickets_People_Flights tpf){sess.delete(tpf);} // Use the session to delete the Object tpf from database
 
-    }
+
 
     // User - LOOKUP a ticket
-    public static Object lookUpTicket (int ticket_id id){
+    //public static Tickets_People_Flights lookUpTicket (int ticket_id){
         //get tickets_people_flights
-        sess.getTicket(tpf);
-        return tickets_people_flights;
-    }
+        //sess.get(Tickets_People_Flights,ticket_id);
+        //return Tickets_People_Flights;
+    //}
 
     // Pilot - LOCK ability TODO lookup how lock works
     public static void lockoutCancel (int flight_id){
-        sess.lock(flight_id); // lock the ability to cancel flight for Users and Admin or BuyNewTickets
+        //sess.lock(flight_id); // lock the ability to cancel flight for Users and Admin or BuyNewTickets
     }
 }
--- Project1-Backend/src/main/java/servlets/ServletTickets.java
+++ Project1-Backend/src/main/java/servlets/ServletTickets.java
@@ -1,31 +1,35 @@
 package servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import daos.TicketDAO;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class ServletTickets extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
+        /**
+         * The following is just testing my javascript to make sure it is working
+         * like I think it is...
+         */
+        InputStream requestBody = req.getInputStream();
+        Scanner sc = new Scanner(requestBody, StandardCharsets.UTF_8.name());
+        String jsonText = sc.useDelimiter("\\A").next();
+        System.out.println("DEBUG - JSON Text: " + jsonText);
+        ObjectMapper mapper = new ObjectMapper();
+        Integer payload = mapper.readValue(jsonText, Integer.class);
+
         //write logic to create a ticket here
 
-        //write logic to delete a ticket here
 
-        String string_ticket_id = req.getParameter("ticket_id");
-        int ticket_id = Integer.parseInt(string_ticket_id);
-        try {
-            TicketDAO dao = new TicketDAO();
-            int flight_id = dao.delete(ticket_id);
-            resp.getWriter().println("Ticket #: " + ticket_id + " has almost been cancelled");
-            resp.getWriter().print("You no longer have a ticket for flight number: " + flight_id);
-            resp.setStatus(200);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //write logic to delete a ticket here
 
         //write logic to delete all tickets for a given flight here
     }
