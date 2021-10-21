08e46603dabec23a57d2cb9101a297fe992cb5e2
andrewdpetersen <andrew.petersen@revature.net>
2021-10-21 11:11:32 -0500

    hibernate manager added

---
+++ Project1-Backend/src/main/java/services/HibernateManagement.java
@@ -0,0 +1,28 @@
+package services;
+
+import models.Flights;
+import models.People;
+import models.Tickets_People_Flights;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateManagement {
+    private Session session;
+
+    public Session getSession() {
+        if(session==null){
+            Configuration config = new Configuration();
+            config.addAnnotatedClass(Flights.class);
+            config.addAnnotatedClass(People.class);
+            config.addAnnotatedClass(Tickets_People_Flights.class);
+            SessionFactory sessionFactory = config.buildSessionFactory();
+            session = sessionFactory.openSession();
+        }
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+}
