ab82bd7849aff840d80901712783c8c94c714e00
JasonWozniak <49988328+JasonWozniak@users.noreply.github.com>
2021-10-22 12:13:15 -0700

    added 2/4 user portal scripts 40 minutes to go

---
+++ Project1-Backend/src/main/FrontEnd/JavaScript/UserJS/userPortalCheckin.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("user_checkin");
+form.addEventListener("submit",function(event) {
+    event.preventDefault();//prevents the default "submit" event
+    const checkinTicketId = form.querySelector("#checkin_ticket_id");
+    SubmitForm();
+});
+
+async function SubmitForm() {
+    let object = {//creates an object in JSON format
+        "ctid": checkinTicketId.value,//key is the variable we are assigning the value to
+
+    }
+    let response = await fetch("http://localhost:8080/Project1-Backend/tickets", {
+        method: "POST",
+        headers: {"Content-Type": "application/json",
+        "Servlet-action": "UserCheckin"
+        },
+        //one to add an object, one to update, one to delete, one to get one object, one to get a list
+        body: JSON.stringify(object)//makes the json into a string to send
+    });
+}
+
+// TODO: add response logic if necessary - return CHECKED IN for Flight bbbbxxxxx
+//The only times we need a response: user-Flights, admin-Manifest, admin-Flights
+
+//let json = response.json();
+//And the response logic goes here
+
+
