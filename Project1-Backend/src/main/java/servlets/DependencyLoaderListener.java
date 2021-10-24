84a7b8dc1c4ad9f92597c7d79104d4f22cc3f312
andrewdpetersen <andrew.petersen@revature.net>
2021-10-24 12:23:40 -0500

    DependencyLoaderListener updated with session and sessionFactory initialization

--- Project1-Backend/src/main/java/servlets/DependencyLoaderListener.java
+++ Project1-Backend/src/main/java/servlets/DependencyLoaderListener.java
@@ -1,5 +1,11 @@
 package servlets;
 
+import models.Flights;
+import models.People;
+import models.Tickets_People_Flights;
+import org.hibernate.cfg.Configuration;
+import services.HibernateManagement;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -7,11 +13,17 @@ public class DependencyLoaderListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        Configuration config = new Configuration();
+        config.addAnnotatedClass(People.class);
+        config.addAnnotatedClass(Flights.class);
+        config.addAnnotatedClass(Tickets_People_Flights.class);
 
+        HibernateManagement.setSessionFactory(config.buildSessionFactory());
+        HibernateManagement.setSession(HibernateManagement.getSessionFactory().openSession());
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-
+        HibernateManagement.getSession().close();
     }
 }
